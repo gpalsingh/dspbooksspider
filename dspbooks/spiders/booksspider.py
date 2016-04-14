336946fb7ac18091726d1805c96f5940fef6db44
Gurkirpal <gurkirpal204@gmail.com>
2016-04-14 06:55:48 +0530

    first commit

---
+++ dspbooks/spiders/booksspider.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+
+import scrapy
+from urllib import unquote
+from urlparse import urljoin
+from dspbooks.items import BookItem
+
+class DSPBooksSpider(scrapy.Spider):
+    name = 'dspbooks'
+    base_url = 'http://serv.yanchick.org/Books/dsp_books/'
+    start_urls = ['http://serv.yanchick.org/Books/dsp_books/']
+
+    def parse(self, response):
+        quoted_links = response.xpath('//ul/li/a/@href').extract()[1:]
+        rel_links = [urljoin(self.base_url, str(unquote(x)))
+                for  x in quoted_links]
+        for link in rel_links:
+            if link.endswith('/'):
+                yield scrapy.Request(url=link)
+            else:
+                yield BookItem(file_urls=[link,])
